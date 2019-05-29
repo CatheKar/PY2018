93524ea5e16f75081b9c53a40e469119d89a4648
Karina <remih_key@mail.ru>
2019-05-29 10:30:57 +0300

    Сайтик

--- flask-learn/st.py
+++ flask-learn/st.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -9,8 +9,27 @@ def hello():
     return render_template('index.html')
 
 
-@app.route('/main/')
+@app.route('/main/', methods = ['GET'])
 def main():
-    return render_template('mane.html')
+    a = request.args.get('a')
+    b = request.args.get('b')
+    c = request.args.get('c')
+
+    result = ""
+    if None in (a, b, c):
+        result = "Не введены данные"
+    else:
+        a = float(a)
+        b = float(b)
+        c = float(c)
+        D = b ** 2 - 4 * a * c
+        if D < 0:
+            result = "Дискриминант меньше 0"
+        else:
+            x1 = (-b + D ** 0.5) / 2
+            x2 = (-b - D ** 0.5) / 2
+            result = "x1 = " + str(x1) + ", x2 = " + str(x2)
+
+    return render_template('mane.html', res=result)
 
 
